44d1867c30a7516bfd3dbfeeceb1918ae0c849b6
Brayan Sanchez <brayan_sanchez99@hotmail.es>
2023-07-03 15:28:49 -0600

    Completing the Home Page

--- src/pages/web/Home/Home.js
+++ src/pages/web/Home/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { Banner, HomeCourses } from "../../../components";
+import {
+  Banner,
+  HomeCourses,
+  HowMyCoursesWorK,
+  Reviews,
+} from "../../../components";
 
 export const Home = () => {
   return (
     <div>
       <Banner />
       <HomeCourses />
+      <HowMyCoursesWorK />
+      <Reviews />
     </div>
   );
 };
